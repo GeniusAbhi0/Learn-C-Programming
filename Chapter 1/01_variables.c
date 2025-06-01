0f37ad572128eccd87c577d82e5cfca3d5af01c7
GeniusAbhi0 <abhijeet2122004@gmail.com>
2025-06-01 23:53:24 +0530

    Delete Chapter 1 directory

--- Chapter 1/01_variables.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-
-    int a ; // Variable declaration
-    a = 9 ; // Variable intialization 
-    printf("%d", a);
-    return 0 ;
-    
-}
--- Chapter 1/LeapYear.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main () {
-    int Year ;
-    printf("Enter a Year:");
-    scanf("%d", &Year);
-    (Year%4==0 && Year%100!=0) || (Year%400==0)?
-        printf("This is leap year"): printf("This is not leap year");
-    return 0;
-}
